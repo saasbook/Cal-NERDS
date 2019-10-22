780859a1fae0069a414a4c88e5e62c80dc151939
Chris Pyles <cpyles@berkeley.edu>
2019-10-22 14:36:50 -0700

    copied files from other repo

---
+++ app/models/schedule.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+class Schedule < ActiveRecord::Base
+	belongs_to :user
+
+	def self.parse_times_strings schedule
+		times = {}
+		for day in ["mon", "tue", "wed", "thu", "fri"]
+			sym = "#{day}_times".to_sym
+			times_string = schedule.send sym
+			if !times_string.nil?
+				day_times = JSON.parse times_string
+				times[day.to_sym] = day_times
+			else
+				times[day.to_sym] = []
+			end
+		end
+		return times
+	end
+end
