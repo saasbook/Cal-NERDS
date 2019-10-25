78b2732a1af50939932c9f9505c02588e8e6c040
Chris Pyles <cpyles@berkeley.edu>
2019-10-25 13:10:42 -0700

    prevented from looking at users while logged out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_current_user
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -65,6 +66,13 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_current_user
+      @current_user ||= session[:user_id]
+      if @current_user.nil?
+        redirect_to root_path
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
