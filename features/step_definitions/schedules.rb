18dd79dcc68777419d76186c71c607c49ba21dec
Ubuntu <ubuntu@ip-172-31-30-151.us-west-2.compute.internal>
2019-11-02 00:53:20 +0000

    basic cucumber tasks

---
+++ features/step_definitions/schedules.rb
@@ -0,0 +1,19 @@
+When /^I select the following times:$/ do |table|
+    puts 'success'
+end
+
+Then /^I should have another schedule$/ do 
+end
+
+Then /^it should have the correct times$/ do
+end
+
+Then /^it should have no times$/ do
+end
+
+When /^I update the schedule:$/ do |table|
+end
+
+
+Then /^my schedule should have the following times:$/ do |table|
+end
