e2a7e3e3a8a1dff37421713a8843be78101f6212
Chris Pyles <cpyles@berkeley.edu>
2019-11-01 17:32:34 -0700

    switched to dev sqlite db

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20191019195244) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "schedules", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
