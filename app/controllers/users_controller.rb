357296aeabd0f79ee36569751fbd8a0599eb1417
Chris Pyles <cpyles@berkeley.edu>
2019-11-11 18:41:14 -0800

    working on upping codecov

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,6 +79,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:state, :code, :scope, :authuser, :hd, :session_state, :prompt, :provider, :uid)
+      params.require(:user).permit(:state, :code, :scope, :authuser, :hd, :session_state, :prompt, :provider, :uid, :name, :email, :auth, :admin)
     end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,11 +17,49 @@ RSpec.describe UsersController, type: :controller do
 		end
 
 		it 'assigns @users' do
-			expect(User).to receive(:all).and_return([@user1, @user2])
-			expect(User).to receive(:all).and_return([@user1, @user2])
 			get :index
 			expect(assigns(:users)).not_to be_nil
 			expect(assigns(:users)).to eq(User.all)
+		end
+	end
+
+	describe 'GET new' do
+		before :each do
+			@user1 = User.create(id: 1, name: 'John Doe', email: 'jdoe@berkeley.edu', auth: true, admin: true)
+			@user2 = User.create(id: 2, name: 'Jane Doe', email: 'jdoe2@berkeley.edu', auth: true, admin: false)
+			session[:user_id] = 1
+			expect(User).to receive(:find).with(1).and_return(@user1)
+		end
+
+		it 'should call User#new' do
+			expect(User).to receive(:new)
+			get :new
+		end
+
+		it 'should assign @user' do
+			get :new
+			expect(assigns(:user)).not_to be_nil
+		end
+	end
+
+	describe 'POST create' do
+		before :each do
+			@user1 = User.create(id: 1, name: 'John Doe', email: 'jdoe@berkeley.edu', auth: true, admin: true)
+			session[:user_id] = 1
+			post :create, {user: {name: "Jane Doe", email: "jdoe2@berkeley.edu"}}
+		end
+
+		it 'should assign @user' do
+			expect(assigns(:user)).not_to be_nil
+		end
+
+		it 'should create a new user' do
+			expect(User.all.length).to eq(2)
+			expect(User.find(2).name).to eq("Jane Doe")
+		end
+
+		it 'should redict to the user page' do
+			expect(response).to redirect_to(assigns(:user))
 		end
 	end
 end
