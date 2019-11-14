ff3aecf1c13517a465c7a103b6b57c83c3dbb4c2
Chris Pyles <cpyles@berkeley.edu>
2019-11-13 17:01:45 -0800

    finished landing page fr

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'cpyles@berkeley.edu', name: 'Christopher Pyles', id: 1, auth: true, admin: true)
