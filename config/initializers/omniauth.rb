bce56d9546c73a15f6d1d3ced796ba0ee619cb2c
Chris Pyles <cpyles@berkeley.edu>
2019-11-14 15:21:39 -0800

    fixed callback uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {
-    redirect_uri: "http://localhost:3000/auth/google_oauth2/callback",
+    redirect_uri: "https://damp-brushlands-79654.herokuapp.com/auth/google_oauth2/callback",
     skip_jwt: true,
     :provider_ignores_state => true
   }
