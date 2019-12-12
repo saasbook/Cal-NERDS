39515f1fc9d90263289686e15722d0f17f45035c
Chris Pyles <cpyles@berkeley.edu>
2019-12-11 16:31:33 -0800

    tests

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -29,4 +29,22 @@ RSpec.describe Schedule, type: :model do
   		})
   	end
   end
+
+  describe 'calculate_hours' do
+    before :each do
+      @schedules = [
+        Schedule.create(id: 1, start_date: Date.parse('2019-12-02'), user_id: 3, mon_times: "[1000, 1030, 1100, 1130]", wed_times: "[1000, 1030, 1100, 1600, 1630]"),
+        Schedule.create(id: 2, start_date: Date.parse('2019-12-02'), user_id: 3, mon_times: "[1000, 1030, 1100, 1130]"),
+        Schedule.create(id: 3, start_date: Date.parse('2019-12-09'), user_id: 3, mon_times: "[1000, 1030, 1100, 1130]"),
+        Schedule.create(id: 4, start_date: Date.parse('2019-12-16'), user_id: 3, mon_times: "[1000, 1030, 1100, 1130]"),
+        Schedule.create(id: 5, start_date: Date.parse('2019-12-09'), user_id: 3, mon_times: "[1000, 1030, 1100, 1130]"),
+      ]
+    end
+
+    it 'should calculate hours' do
+      expect(Schedule.calculate_hours Date.parse('2019-12-02'), Date.parse('2019-12-08')).to eq(6.5)
+      expect(Schedule.calculate_hours Date.parse('2019-12-02'), Date.parse('2019-12-15')).to eq(10.5)
+      expect(Schedule.calculate_hours Date.parse('2019-12-09'), Date.parse('2019-12-17')).to eq(6)
+    end
+  end
 end
